ead213e83d69417b92c81def712662ff598ee428
pecavalc <pecavalc@student.42berlin.de>
2025-09-07 14:51:01 +0200

    Copied the sources of Libft-v2.0.0 and minilibx-linux libraries into the libs folder.

---
+++ libs/Libft-2.0.0/srcs/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 19:03:32 by pecavalc          #+#    #+#             */
+/*   Updated: 2025/09/06 19:12:51 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stddef.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (&((void *)s)[i]);
+		i++;
+	}
+	return (NULL);
+}
