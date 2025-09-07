f17d228d061d27427f2930a6011635c5ffb81888
pecavalc <pecavalc@student.42berlin.de>
2025-09-07 15:28:48 +0200

    Created empty main.c under srcs, incl. planned next steps.

---
+++ srcs/main.c
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 15:24:38 by pecavalc          #+#    #+#             */
+/*   Updated: 2025/09/07 15:25:50 by pecavalc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// TODO: 
+// - Write and compile a program that just initializes and destroys minilibx.
+// - Incl. Makefile
