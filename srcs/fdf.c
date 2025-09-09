a572fbae20667509514f277908ba75d07f083b2e
pecavalc <pecavalc@student.42berlin.de>
2025-09-09 17:03:43 +0200

    Added check for number of arguments.

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,21 +6,26 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 15:24:38 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/09/09 15:57:29 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/09/09 16:57:42 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include "libft.h"
 
 int main(int argc, char **argv)
 {
 	t_map	*map;
 
+	if (argc != 2)
+	{
+		ft_putstr_fd("main.c: invalid number of input arguments.", 2);
+		return (1);
+	}
 	map = load_map(argv[1]);
+
 	//TODO: Handle NULL map returned
 
-
-    return 0;
+	return 0;
 }
-
 
