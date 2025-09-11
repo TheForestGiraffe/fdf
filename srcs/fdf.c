0409a16290c354130f539e16fb3236d7c2fb18c0
pecavalc <pecavalc@student.42berlin.de>
2025-09-11 13:02:00 +0200

    Added temporary print_map test function

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,12 +6,14 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 15:24:38 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/09/09 16:57:42 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/09/11 12:59:29 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 #include "libft.h"
+
+void	print_map(t_map *map);
 
 int main(int argc, char **argv)
 {
@@ -24,8 +26,31 @@ int main(int argc, char **argv)
 	}
 	map = load_map(argv[1]);
 
-	//TODO: Handle NULL map returned
+	print_map(map); //TODO: remove test
 
 	return 0;
+}
+
+void	print_map(t_map *map)
+{
+	int	x;
+	int	y;
+
+	ft_printf("\n");
+	ft_printf("Nr. of columns: %i\n"), map->nr_columns);
+	ft_printf("Nr. of rows: %i\n", map->nr_rows);
+	ft_printf("Vertices:\n");
+	y = 0;
+	while (y <= map->nr_rows)
+	{
+		x = 0;
+		while (x <= map->nr_columns)
+		{
+			ft_printf("%i ", map->vertices[x][y].z);
+			x++;
+		}
+		ft_printf("\n");
+		y++;
+	}	
 }
 
