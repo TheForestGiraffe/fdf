9abf2947f0c3ca4d56fcf94d02352a1335b0fce4
pecavalc <pecavalc@student.42berlin.de>
2025-09-11 20:04:52 +0200

    Fixed test print_map function

--- srcs/fdf.c
+++ srcs/fdf.c
@@ -6,7 +6,7 @@
 /*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 15:24:38 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/09/11 12:59:29 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/09/11 19:14:23 by pecavalc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,16 +37,16 @@ void	print_map(t_map *map)
 	int	y;
 
 	ft_printf("\n");
-	ft_printf("Nr. of columns: %i\n"), map->nr_columns);
+	ft_printf("Nr. of columns: %i\n", map->nr_columns);
 	ft_printf("Nr. of rows: %i\n", map->nr_rows);
 	ft_printf("Vertices:\n");
 	y = 0;
-	while (y <= map->nr_rows)
+	while (y < map->nr_rows)
 	{
 		x = 0;
-		while (x <= map->nr_columns)
+		while (x < map->nr_columns)
 		{
-			ft_printf("%i ", map->vertices[x][y].z);
+			ft_printf("%i ", map->vertices[y][x].z);
 			x++;
 		}
 		ft_printf("\n");
