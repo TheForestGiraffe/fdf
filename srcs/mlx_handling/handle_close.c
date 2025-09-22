19c1c6660114563049344554c242f75cd991c16e
Pedro Lima <pedrolima.ep@gmail.com>
2025-09-22 07:06:59 +0000

    Now freeing also mlx img. Added wrpas to prevent passing null pointers to mlx destroy.

--- srcs/mlx_handling/handle_close.c
+++ srcs/mlx_handling/handle_close.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_close.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pecavalc <pecavalc@student.42berlin.de>    +#+  +:+       +#+        */
+/*   By: plima <plima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/16 10:55:28 by pecavalc          #+#    #+#             */
-/*   Updated: 2025/09/20 21:36:40 by pecavalc         ###   ########.fr       */
+/*   Updated: 2025/09/22 07:03:26 by plima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,12 @@ int	handle_close(void *param)
 
 	app = (t_app *)param;
 	free_models(app->model, app->projection);
-	mlx_destroy_window(app->mlx, app->window);
-	mlx_destroy_display(app->mlx);
+	if (app->img.img)
+		mlx_destroy_image(app->mlx, app->img.img);
+	if (app->window)
+		mlx_destroy_window(app->mlx, app->window);
+	if (app->mlx)
+		mlx_destroy_display(app->mlx);
 	free(app->mlx);
 	exit(EXIT_SUCCESS);
 	return (0);
