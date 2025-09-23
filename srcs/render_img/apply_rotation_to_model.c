7fccd2658e579500bfbd7031794b993a1bdd65f8
Pedro Lima <pedrolima.ep@gmail.com>
2025-09-23 16:56:46 +0000

    WIP in rotation

--- srcs/render_img/apply_rotation_to_model.c
+++ srcs/render_img/apply_rotation_to_model.c
@@ -6,7 +6,7 @@
 /*   By: plima <plima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 12:47:58 by plima             #+#    #+#             */
-/*   Updated: 2025/09/22 14:01:16 by plima            ###   ########.fr       */
+/*   Updated: 2025/09/23 16:55:18 by plima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,37 @@
 void    apply_rotation_to_model(t_app *app)
 {
     (void)app;
+/*
+    int		x;
+	int		y;
+
+	y = 0;
+	while (y < app->model->rows)
+	{
+		x = 0;
+		while (x < app->model->columns)
+		{
+			app->projection->vertices[y][x].x = app->view.shift_x;
+			app->projection->vertices[y][x].y += app->view.shift_y;
+			x++;
+		}
+		y++;
+	}
     //TODO:
     // rotate_around_x_axis(...);
     // rotate_around_y_axis(...);
+    */
 }
+
+/*
+void    rotate_around_x(int x, int y, t_app *app)
+{
+    int z;
+    int theta;
+
+    z = app->model->vertices[y][x].z;
+    theta = app->view.rot_angle_x;
+    app->model->vertices[y][x].y = y * cos(theta) - z * sin(theta);
+    app->model ->vertices[y][x].z = y * sin(theta) + z * cos(theta);
+}
+*/
