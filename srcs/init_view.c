9de50b551f19ecb6da87bc2c47de8509280f7179
Pedro Lima <pedrolima.ep@gmail.com>
2026-03-04 18:08:04 +0100

    Initialize window with 1980 x1080 resolution. Change line color to white.

--- srcs/init_view.c
+++ srcs/init_view.c
@@ -15,14 +15,14 @@
 
 void	init_view(t_view *view)
 {
-	view->width = 1024;
-	view->height = 768;
+	view->width = 1980;
+	view->height = 1080;
 	view->zoom = 20;
 	view->zoom_step = 1;
 	view->shift_x = 0;
 	view->shift_y = 0;
-	view->translation_step = 10;
-	view->color = 0xffbb00;
+	view->translation_step = 5;
+	view->color = 0xffffff;
 	view->scale_z = 1;
 	view->currently_dragging_with_left_mouse_button = false;
 	view->currently_dragging_with_middle_mouse_button = false;
@@ -31,7 +31,7 @@ void	init_view(t_view *view)
 	view->rot_angle_x = 0;
 	view->rot_angle_y = 0;
 	view->rot_angle_z = 0;
-	view->rot_step = 0.01;
+	view->rot_step = 0.1;
 	view->projection_type = ISO;
 	view->is_auto_rotate_on = false;
 }
